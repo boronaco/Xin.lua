8f31fe9a0565e1a2dbb67e3a6c3569c4f243b8ec
boronaco <julianobordon@gmail.com>
2017-07-01 22:13:44 -0300

    Update ahri8.lua

--- ahri8.lua
+++ ahri8.lua
@@ -1,7 +1,7 @@
 local KoreanChamps = {"Ahri"}
 if not table.contains(KoreanChamps, myHero.charName)  then print("" ..myHero.charName.. " Is Not (Yet) Supported") return end
 
-local KoreanMechanics = MenuElement({type = MENU, id = "KoreanMechanics", name = "Korean Mechanics Reborn })
+local KoreanMechanics = MenuElement({type = MENU, id = "KoreanMechanics", name = "Korean Mechanics Reborn" })
 KoreanMechanics:MenuElement({type = MENU, id = "Combo", name = "Korean Combo Settings"})
 KoreanMechanics:MenuElement({type = MENU, id = "Harass", name = "Korean Harass Settings"})
 KoreanMechanics:MenuElement({type = MENU, id = "Clear", name = "Clear Settings"})
