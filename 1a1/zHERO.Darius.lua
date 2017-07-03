af89829528c5641ee3bf68a02eac9390d13f4714
boronaco <julianobordon@gmail.com>
2017-07-03 16:31:31 -0300

    Update and rename ahri0brand0blits0darius0diana0kog0jhin.lua to zHERO.Darius.lua

--- 1a1/zHERO.Darius.lua
+++ 1a1/zHERO.Darius.lua
@@ -1,4 +1,4 @@
-local KoreanChamps = {"Ahri", "Brand", "Blitzcrank", "Darius", "Diana", "KogMaw", "Jhin"}
+local KoreanChamps = {"Darius", "Diana}
 if not table.contains(KoreanChamps, myHero.charName)  then print("" ..myHero.charName.. " Is Not (Yet) Supported") return end
 
 local KoreanMechanics = MenuElement({type = MENU, id = "KoreanMechanics", name = "Korean Mechanics Reborn | "})
