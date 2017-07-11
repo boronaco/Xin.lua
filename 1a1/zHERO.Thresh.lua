78f9982625ddf536b2b4da844e3a3ba612687c66
boronaco <julianobordon@gmail.com>
2017-07-10 22:22:28 -0300

    Update and rename ThreshTocsin.lua to zHERO.Thresh.lua

--- 1a1/zHERO.Thresh.lua
+++ 1a1/zHERO.Thresh.lua
@@ -1,86 +1,3 @@
---                                              :   :    i@nOrLT          .   ;:        .,                                                            
---                                            :s.  .      6;XZst              .                                                                       
---                                          .rr; ;:       .rZbBF   .    ,  . .    .     ,    ;rr                                                      
---                                          r.  ;;  ..     ,Z#sj  .,   .  .      ,      ,.   .,s.                                                     
---                                     ..  i:  T; . ,.   . r6FS.  ;      ,.     ..     ..      r                                                      
---                                   r;@Bd6HO;r5   .:,     PU:r;  .     ..         ;#@r       .r                                                      
---                                  5B8rcbHdr;,     .     i@i:s,       :.     ;,,iEDOF@Fr     ;;                                                      
---                                  x@;:: CU;.    ..   .rTOdCt,    .  :... ,cZB@BBsrZZ2@0c:  ,r                                                       
---                                  bB0t,;@r:     :  LBUbHgPP;    :   .   ;P@;CbS;   sjF@E. .7.                                                       
---                                   .  r@Br,    .: 5@;:r@UD5:  . .      .g@d;;2.  sFcP6c,  :.       .                                                
---                                     rFBS;r .  :..gr;jsrTDr ..        . ;;DO;.  ,,@HX;:  :                                                          
---                                     ;tDZc .,     r;ELr.;   ..       ,    FdT; .     .;  i,:                                                        
---                                    ;s0E;r ,.     ;T@x;;.  :.       , ,  .6g5r;    ;s,    r;     ;c                                                 
---                              ..    rs2;;..,      rPg5r.  .;  ... .,   .rPgdZ.r. ..x;    ;,    .;7r                                                 
---                              rS.   EOrs;..      ;nEtOc   .   : .,     rgZirZsr .. j   ;i     ;r.:;                                                 
---                              ;i;  C8Ur;        jUOLUgi   :  ..,   ..;OdPF;.c;    ..  ;r    rxr;:;      ;,                                          
---                              ;,; Xd8Err    .  jXxO5SEx, .,     ,c0gHB@Zc;::.     .  r;   .FC:          X:                                          
---                             .rr  rUZZ;; .     gTCDgXr;;       sddsEEdS.       .,: .r.    ic           ,t,                                          
---                             .cr  ;DC0;    ,.  XdEc:;2r  .:   jH5C6xCc      .:.., .r.                  rL:                                          
---                       .   . ;s   ;nOss.   ,. r@dT tgc.  ;   j8nsrjHi    .. .:   .r                    rxC                                          
---                             rc   crXCr;       T@;r#S;,  :  .cPs72;r;   ..  ..;;.r                    r;DL                                          
---                             ;.  ;r i@r;,      :d;xB:;;  ;   F6t;F;i.  ..  ..:;:i   ;s               ;LrB;                                          
---                                 r  r#d,:   ;  gHr;@c.r. .. ;X#sri:   :,  ,,  .,   i@;              ;jrsZ                                           
---                                .;  ;#P6i: ,. .bCCrcj.r,   ;SnFs7:   ,,   ,;r;r; .;5#              ;c;,X:                                           
---                            .   i. ,.r##6r   .dnnCZUgZ:r  ;DOSSnr   ,:  ;;rFrrc;,..,ri;::;;it;   .rr;;;F5                                           
---                                r.    P8Xs:  :rHP0O0txic  sgHCXH;  .;. ..;PbO@#PsrE622crrT5i:LnSttir;75OB:                                          
---                                r  ;  r8XZ#   7BUCFS2 ;c  tircDLr; .    ;5j;U08B@D0SdDDbd0g7; r2r :;;t#8r                                           
---                                r .r   ;L#Bc; r8xCsngj.:.F5rr ;2:  .     Tr  C#X2nindOxXS6EbBP0@s.sic@#.                                            
---                               ;E .c    .X@gj,:HSttsdS  0@XOt rE     .   .;      DgZ8UFOrrdtcjEHTdPi@6                                              
---                               ;n  O     2BD6L:is55CO6,gBgsXt.;:;;   , .         L8DXU;t7cBFCDB;ZFrbg                                               
---                                i  c:     nPZ8ExsLxx2208OF5UC;ir ;r .. .:        c0SZ:,LsxPHP#5rc;tL                                                
---                                r; .c, ,  cZcxB@jt52SgDOCsjSiC;U,:r ;  :.:iL.   .6FxcjtxgZH0c;;rrE#:                                                
---                                 j  .. ., ;ECjn8252SStFsssLxgC0c.r  , ,.  ,iF   EDO#BSCx0DdOi ;;;LPZxcF2;                                           
---                            ,    ,r     .  HnLLPnCSSt2nSFgZZiis;r          ,i  CdcSUjrcZxCX;;iri;.;riOO,        ;ccC;                               
---                            #0.   :;  ,,  .B2jFOSSOCOEnC2gnrxPs;cr,        .  :8jLnCcriC2nUCb8B@B.rxbBj  ,::   5O2@B@7                              
---                            ,@Tr.  ;X  ;   PtCFZn#Xg#Hx2nZ  F8DFrr;;   : ,    5ZnPj2jsXHCxFxCFb8;;gH8#8Bd8D@PcOCinOb#@B@#L  :r;                     
---                             LU.r7r,tr;    idxZOj5XUnrgXn;:X8XdLF;;r  ,. .:  ,Hg#0Lr0HFrt8X;Ub5.:rFD8D0D#6H#@Bsrng8gOOZ0@BdOgPd2;;                  
---                              ni.rU@0#   .  SB;rL,r@H;;TScZLritir,c;  ; .    :B; CCSxrj0HFjUs;.r508@DbdbbbB@xjtXH8EUEDE0#EgPXHg0886Zc;.             
---            ,            ,.    gjsi0@P;  .  xB5rnScPXgjjxFL .sc;     .;:  .   :   @Px6#OCrr,,;t0@B##8DDD#@@rrnt8@###H#d#0nZZUXDd#Hd0D6Exs7r:        
---            ;S           rCi;. ,F;r;r@DT ,r. OB@DESFEx2EXZris@;;  :  ,,  .;      PBHPCLUOn.:rcS@Bbddd##88@ri#Cg@#8dXsTcnP8d8B@#DH0PD6d6D66PgFLr;..  
---             ;#:      ...  ;r7ii,  C.r@Ux.:r   ;rHZs6dUOPt;bXc;;,,  ..   .;, :: .gs5jsncxnxFT;rUd88##HD8@riBZX@b02s7iijUBBBdDZFPHPd6Xg#H##D0HHHZZUxr
---              i#;              :;,  F E0ni  ,r   DBUZHZDgTrid5; ,  ..    ....jx:  .X5xnrr7S@dr,;cFU6D##@L dPSb8PtciTCnss5CjjjXdD6PHb#8##6EE0OgggggUT
---               rEr.     .          rs LBcFi  ,L  ;@UD6c2827 ;2    ;, .. .   rr   :ULn6Fci5@x :crLLF6d#805cFFFP82Lc555s5LLsirgB@XxZ#DgnOZ6D#b8PZSgDg5
---                ;F;,;,          .rC7 ;6XHn0;  ,,   rSx0;.@U      ;.  ,,:  .:.  rnHH#nsZbD87 ,L5UdB8##B6Xr;XPSDD0FUtLTtSSsL7ZB88DXHPZsSjxSx26Pd8B8B#H
---                 ,U; .;;:    cZCCr;:;bU7sg6gs       . .2  L  ,        .  ..   F8#dEsCB@b0;.rOPF8@##b@b5: rEd#B62cjLLnFitbLig#CDbbP5xZSn2SxxL2XPE#88O
---                  .Ec  ;r;rcEr   iPUZFCtCC0D0U     .    .     .,..     ,,,  ;PPSTCSDBU;.;TH@Hs0@db@dcr;:iXD8X5rrsOXOti;5XccLs2bBgESCFZU2sCLsTgFZ8bgx
---                    t6i;ri;.rZ8X6dZ5LZO6Zsii2@. .   ,. . T .0 :cBr  ;ir. .iEEx25O2HB. .sD@Zr;E@8B#Hr;rtCZ8BUcrtd8tsc7riZ02r7F8@HX062n5UssnFxE6d##PnC
---                     ;xP6xc;,iXDdd0OSCSSLTstir  is;. . rnPc,ZcUCEsr5r.  ;DPFx0#6X8B. rH@67rnX#FCcjn5cst0BbLrr0B@CrrirrsgP8nE8B#D6EOFUPjn8@8BBdB8UXEZ
---                       i;  cUr:  ;6B0LsUOZgdj.    .,r;  rb@#Hx@nis:   .nBXHd88b#L;.,FB@BBD0F8#;rx6nLx2BDsrTU8HxrrrTciEOXP#B88#bHbbPUP#F2X0PXU2FtSx5s
---                            i@Er;:   6B5LHHOD@#jsi:  ;.  rcSZ6; .    rPB6@02ZX,i;:c08@8@i   rPsPbTtFnBgCC#8@EcrircTcgBXU#B#8D#d##b8EUbxnOXCCs5LCjTsc
---                             ;BEFrnr,.icSxPbdgdPU2DDs.      .  ..  :gUgP0E0XBO  sD@B88@dr:rjnbdTS5LX@8EB@8BZL77rjXCt8UP08##D#dDHd#8###ZEXCSCx5xtC5si
---                                iXBOjE7;r7rrsHXZUFCHdDPj. .:,:    :gdgTsC252P: rD@b@@#gB8Dd6d8H0nPd888#88@DrrrridHxF8UP#bbHDd#H#6b6#8BDDUxCCxFtxsss5
---                                 ,5g   tc.,   FO7 jSTn6bHL;    ,. .TxTxO8EO#x CBBB@DjrLZ##UO6UH8B8@HgFUtcirrccc7Z8DL@d###06HDd###d66#0gddsxStLStsttc
---                                   :i,,#B86gtr:   :Bg5tSOPXj. ;;.;UsL0#80#B@;rB@B@X, :rsLrrL56B8HgSn25ir;riicsi;rBdXb8#Dd#DbDd6dDbDE6ZssEUC5sCjtSn52
---                                   2@H@B@B@B@BECs; ;SZssx2g8@r  s0sS8gsH@#c.;Cxd@C  cg6r7FXcCBB2xLLUx7ritnxiLgT:ib#bD#bd#D##8E2d#8B0BHnZ0Pd#8##D8bHE
---                                   jBEsTPBD8#88@BBSr  rUjgHn@Bir6ccdSL62.  C@@@@s r#BbrinZCPB8CCjjLtSUHdO6FrrP8Cx#bb##d#D#6xXDcxX#0PUd88#8b@B@B@8d0Z
---                                    @HDtOB@8BB@DB@@B0, r. 08O8ddn;tj56D; ;bB@X;r.X@ZEOSUZHBB6LsccsSX@#UTxZt;;7@B6#8DbD#b86O5OUsCU2XntC2CFUOnXE668bdE
---                                   xB@DdXZ5T2P88B@D@@@XSr  ObPs08iTH7  ,s@B@: :s0272nSZ8B@0xcccxgD#POFZDb0r;r5tU@86DD##B0gEZSFbg5FP0ZgE#H0XZZXxCtnXF
---                                  @BH#BBDLj;rUXBB;  rH@Bbt. iBg28P@D .rE@@U ;E6Or:Csn6@BDxjrLX#8DPnTUPBBB6gsj2LT8#d#B8HZxFgxZBbxXtiriTLcr;rrixd8dF52
---                              x@2HB#gnn0B@#EsTjFHZ5; .8@B@8S.:OD8@8;;O8@BHrO#ZtL2SCHB@dSCnsZ8@#6F5CEB@BELCx5CxC#B8P6Xg0ZtngtcXj;,              .;txF
---                              F#xngD#iS0#8@8bOF5FSZ#0E#0bB@B@L:cPn.768@BD5CUFigSsX@B8XOOnFbB#OSjsX@B8ScicLtLcj6B80X2F5SUFn#;                        
---     .                      ;tb6EdODdsxd0HPB#8BXjO2gBj:rcUH@B@bi,i;0BB#@L;Cs.rZFd@8@6d8XxB8EjirnB@Hxr;cnjcsxOP8B88H6EFnOr;.                         
---                  .P,     @B@BbHbdOHBLZ0nEUOB8BD#6g#6Lcr,  ,CB@@OgZB@DdBXrOc5nZd8XDBBDxLSZFcjU@@#TrrSDB@DgDbBbbd60DHgF0;,                           
---                  ,@BZ, :@B@8D6DDb0d8HZP;0#Z5PD#6##BDUEdZSrr  ;@B@#@B@8@BBPn525FT28@6Ltn5sj5n@BSriiOBP52UXx5LTiiicttjn7,                            
---               ,  rB@B@B@BB#B88D#F#gUnP@;;gD#XP08DPZdb8gCE@#nr.U@i:;irrnBB8FssTcsZ@8CCZFOZnx5cjcrrcLCUOZXZXOgg0gXSL5HC;                             
---               ;; S@8bbBbd#bD888DcEXs7TBb;c6gHB@B@bDPPH86SCZgES@O rHSTcTgBBXris6bBBZTLctLLjcitCStxO8B@8#d8@@B@8DZ55dO;                              
---              .; c@B86#D#d#dDDbd80Z#nXFj0P2UZUU5OOBb#d6E8DZjjn8BZ:DgE2SgU0@888@B@BdgHPEFgPFx0FUg8@@BD6#88bb##HH6#bb#Z;                              
---               ;.X@Bd8DbDb##D8#bH8dB06802PEUFZXUsi6@D#B#gB8PnCH@86FUxC5UOP#@BP;:,cZ@8nS5CFSgHB@@B@BBbBB8bbDb##d0gPn2sj;                             
---              : ,B@8BB8#8#b#b#8#DHbbB8B8DXns0U5XD0#88sZ88dB8HXB0#BBsccstP6@BP,.;r;rs#Ond0xCsstSOEOPH8b#b8d#D8b#EFxZnUsj.                            
---          ;c  ; S@88#8#8#8D##B8BD#b#0XZ88d6ggd0UngPZX#Pg#B#8B@d;;D@@PZZd8@BP7tnUd8Hgd@P#DEFOSCs55C5FDBD#b8#8bB#HOjisxSr;                            
---          rr;r.;@@8#8#bb8###bDDd88dPHPntngdPg#@B@B@XEO6db88bbB0r,;t6@B@BBdSSXscCX60nP80FFxUCUUDB@b@B@D#d#B@b88B88OE2TcO     .                       
---          ;;;U HBB#8#b#8#8###8##Xd#Bbd##P6H@BBs,,:;rSgsFxdH8Bd;;:SFFtnB@Ht6FtsF0OXFZH8B@b8d2XH0HDb8@BD0#8bH#88#8#BB#SDB      .                      
---       ;;   2r.B@D8B@b88B888BD#d6UHdBgO08EgZT,      ,OdDFxdB@Z.:CdnUXtr8@xtCnZOOn5UDB8XLsTLjxC5cjiriO8B@nDd#b8b#dZ0DgPOr                            
---       r;7j:Z ;b8@B@B88B8B8B#D#B#DDbbB8BHCC          ;j#88B@BBisHnxscst8B@#OZ0OFxP#BB8ZEOXFxsjTTii7ri@@iF@88b###6#ZOXXxc7,                          
---     . : r8rx  .rLOH@B@8b#bD#6DDDd8B@6UTZE.           t8#n#BBBBnF2XxUU8BUsOZbB8#88#Dd0dgE2Stx2tsCSOD@#..@BB#8d8XsZOC5FHUT;                          
---    :irg,Tg;jj;...;Tn08@b8##DbD8B@BX.,   .           .:s#USZB@8#B@8#D8@@;cxXb8#bd0ZOnSsxT7sSUXX#DBB@L  jB8#BdD#80EOXnXO6ST                          
---    ::;P;Cj rCHZtr: :iLE@B@8#8@B@BO                   .rODDUDB8DB#Dd##@giH#D8bB#8HPUOCDBB8BB@B806B@;,;rB@886#b#D@8b8@8bOxE                          
---    . :2 ;cr  :rrtSr  ..,c2gE600;                        ,jUT2#8gPPH0#D;L0g0DdP#dDHdXZSg0DgDHdFSBb ,;:s@H6#Dd#gXZEEOUXn#H@PS:                       
---
-
-
-
-
 local ScriptVersion = "v2.0"
 
 local Q = { Range = 1050, Delay = 0.50, Speed = 1200, Width = 70}
@@ -315,36 +232,36 @@ function Thresh:__init()
 end
 
 function Thresh:LoadMenu()
-	Tocsin = MenuElement({type = MENU, id = "ThreshTocsin", name = "ThreshTocsin"})
-	Tocsin:MenuElement({name = "Thresh", drop = {ScriptVersion}, leftIcon = "https://vignette3.wikia.nocookie.net/leagueoflegends/images/b/b5/Thresh_OriginalLoading.jpg"})
+	Tocsin = MenuElement({type = MENU, id = "ThreshTocsin", name = "Thresh"})
+	Tocsin:MenuElement({name = "Thresh", drop = {ScriptVersion}})
 	
 	--Combo
 
 	Tocsin:MenuElement({type = MENU, id = "Combo", name = "Combo Settings"})
-	Tocsin.Combo:MenuElement({id = "Q", name = "Use [Q]", value = true, leftIcon = Qicon})
-	Tocsin.Combo:MenuElement({id = "W", name = "Use [W]", value = true, leftIcon = Wicon})
-	Tocsin.Combo:MenuElement({id = "E", name = "Use [E]", value = true, leftIcon = Eicon})
+	Tocsin.Combo:MenuElement({id = "Q", name = "Use [Q]", value = true})
+	Tocsin.Combo:MenuElement({id = "W", name = "Use [W]", value = true})
+	Tocsin.Combo:MenuElement({id = "E", name = "Use [E]", value = true})
 	Tocsin.Combo:MenuElement({id = "Key", name = "Toggle: E Push-Pull Key", key = string.byte("T"), toggle = true})
-	Tocsin.Combo:MenuElement({id = "R", name = "Use [R]", value = true, leftIcon = Ricon})
+	Tocsin.Combo:MenuElement({id = "R", name = "Use [R]", value = true})
 	Tocsin.Combo:MenuElement({id = "ER", name = "Min enemies to use R", value = 1, min = 1, max = 5})
 
 	--Harass
 
 	Tocsin:MenuElement({type = MENU, id = "Harass", name = "Harass Settings"})
-	Tocsin.Harass:MenuElement({id = "Q", name = "Use [Q]", value = true, leftIcon = Qicon})
-	Tocsin.Harass:MenuElement({id = "E", name = "Use [E]", value = true, leftIcon = Eicon})
+	Tocsin.Harass:MenuElement({id = "Q", name = "Use [Q]", value = false})
+	Tocsin.Harass:MenuElement({id = "E", name = "Use [E]", value = false})
 	Tocsin.Harass:MenuElement({id = "Mana", name = "Min Mana to Harass [%]", value = 0.30, min = 0.05, max = 1, step = 0.01})
 
 	--Flee
 	Tocsin:MenuElement({type = MENU, id = "Flee", name = "Flee or YOLO"})
-	Tocsin.Flee:MenuElement({id = "E", name = "Use [E]", value = true, leftIcon = Eicon})
-	Tocsin.Flee:MenuElement({id = "W", name = "Use [W]", value = true, leftIcon = Wicon})
+	Tocsin.Flee:MenuElement({id = "E", name = "Use [E]", value = false})
+	Tocsin.Flee:MenuElement({id = "W", name = "Use [W]", value = false})
 
 	--Draw
 
 	Tocsin:MenuElement({type = MENU, id = "Draw", name = "Draw Settings"})
-	Tocsin.Draw:MenuElement({id = "Q", name = "Draw [Q] Range", value = true, leftIcon = Qicon})
-	Tocsin.Draw:MenuElement({id = "Push", name = "Push Toggle", value = true})
+	Tocsin.Draw:MenuElement({id = "Q", name = "Draw [Q] Range", value = false})
+	Tocsin.Draw:MenuElement({id = "Push", name = "Push Toggle", value = false})
 end
 
 function Thresh:Tick()
@@ -505,9 +422,9 @@ function Thresh:Draw()
 	if Tocsin.Draw.Push:Value() then
 		local textPos = myHero.pos:To2D()
 		if Tocsin.Combo.Key:Value() then
-			Draw.Text("Push: On", 20, textPos.x - 33, textPos.y + 60, Draw.Color(255, 000, 255, 000)) 
+			Draw.Text(".", 20, textPos.x - 33, textPos.y + 60, Draw.Color(255, 000, 255, 000)) 
 		else
-			Draw.Text("Push: Off", 20, textPos.x - 33, textPos.y + 60, Draw.Color(255, 225, 000, 000)) 
+			Draw.Text("", 20, textPos.x - 33, textPos.y + 60, Draw.Color(255, 225, 000, 000)) 
 		end
 	end
 end
